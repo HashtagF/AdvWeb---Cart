51ecb6cbec52d46f75f198a36420ffd6b320c4db
srvjack39 <32787515+srvjack39@users.noreply.github.com>
2018-05-02 18:30:27 +0700

    x
    x

---
+++ AdvWeb-Cart/src/components/order.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+111
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      product: [],
+      chooseItems: []
+    }
+  },
+  created () {
+    for (i=0; i < 10; I++) {
+      this.product[i] = 'items1'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
