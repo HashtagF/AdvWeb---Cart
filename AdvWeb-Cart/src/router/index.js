a8816d17e89307eb04069aaba51adb2b22ce1a35
HashtagF <flamezaxaou1@gmail.com>
2018-05-02 18:34:48 +0700

    synxcs

--- AdvWeb-Cart/src/router/index.js
+++ AdvWeb-Cart/src/router/index.js
@@ -1,11 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-<<<<<<< HEAD
-import HelloWorld from '@/components/HelloWorld'
-import order from '@/components/order'
-=======
 import Input from '@/components/Input'
->>>>>>> 2339330b08a5cf72714072748c87175feb80a837
+import Order from '@/components/Order'
 
 Vue.use(Router)
 
@@ -13,18 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-<<<<<<< HEAD
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/order',
-      name: 'order',
-      component: order
-=======
       name: 'Input',
       component: Input
->>>>>>> 2339330b08a5cf72714072748c87175feb80a837
+    },
+    {
+      path: '/Order',
+      name: Order,
+      component: Order
     }
   ]
 })
